718eef99e6d62a099bf887b82a916b57565b37ef
Dom <122333281+DomenicGTH@users.noreply.github.com>
2023-02-28 23:18:46 +0000

    created operator functions

---
+++ js.js
@@ -0,0 +1,21 @@
+let numbers = document.querySelectorAll('.number')
+let operators = document.querySelectorAll('.op')
+let display = document.querySelector('.output')
+let firstNum = ''
+let secondNum = ''
+
+function addition() {
+    let addAnswer = firstNum + secondNum
+}
+
+function subtract() {
+    let minusAnswer = firstNum - secondNum
+}
+function multiply() {
+    let multiAnswer = firstNum * secondNum
+}
+
+function divide() {
+    divideAnswer = firstNum / secondNum
+}
+
